f7fe93405a83bdd5d71ba3d62f0b94ed4afe4373
Aziz2525 <aziz.app.developer@gmail.com>
2021-11-29 14:11:14 +0300

    29.11.2021

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const haberTurk = require("./get/haberler/haberturk");
 const cnn = require("./get/haberler/cnn");
+const haberlercom = require("./get/haberler/haberlercom");
 const ntv = require("./get/haberler/ntv");
+const aa = require("./get/haberler/aa");
 const Doviz = require("./get/doviz/index");
 require("dotenv/config");
 
 app.use("/get/haberler/haberturk", haberTurk);
+app.use("/get/haberler/haberlercom", haberlercom);
 app.use("/get/haberler/cnn", cnn);
 app.use("/get/haberler/ntv", ntv);
+app.use("/get/haberler/aa", aa);
 
 app.use("/get/doviz", Doviz);
 
